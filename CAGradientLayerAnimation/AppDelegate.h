c8c6f554b8fbead679234d91c15a03eefce28494
jpz <jiangpengzhi@moqipobing.com>
2019-04-19 20:08:31 +0800

    Initial Commit

---
+++ CAGradientLayerAnimation/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  CAGradientLayerAnimation
+//
+//  Created by jpz on 2019/4/19.
+//  Copyright © 2019 jpz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ CAGradientLayerAnimation/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CAGradientLayerAnimation
+//
+//  Created by jpz on 2019/4/19.
+//  Copyright © 2019 jpz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
